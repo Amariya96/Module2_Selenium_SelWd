65e6dac7a620008e65099fadaac5fddf34af2ccb
Amariya96 <197153@ust-global.com>
2023-11-14 18:13:42 +0530

    14/11/2023

--- Assignments/Program.cs
+++ Assignments/Program.cs
@@ -1,6 +1,20 @@
 ﻿using Assignments;
-AmazoneTest at = new();
+using NUnit.Framework;
+/*AmazoneTest at = new();
 at.InitializeChromeDriver();
 at.TitleTest();
 at.URLTest();
 at.Destruct();
+*/
+LaunchNewBrowser launchNewBrowser = new LaunchNewBrowser();
+try
+{
+    launchNewBrowser.InitializeChromeDriver(); //calling intialize method
+    launchNewBrowser.SearchResultTest(); // calling SearchResulTest  method
+
+}
+catch (AssertionException)
+{
+    Console.WriteLine(" Title Test failed");
+}
+launchNewBrowser.Destruct();
